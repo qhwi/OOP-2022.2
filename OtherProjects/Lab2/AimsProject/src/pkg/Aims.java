4704b278d05795b7536c5061f5a369dd47ed914e
Hwi <nguyenquochuy712@gmail.com>
2023-05-27 07:37:12 +0700

    move file

---
+++ OtherProjects/Lab2/AimsProject/src/pkg/Aims.java
@@ -0,0 +1,27 @@
+package pkg;
+
+public class Aims {
+
+	public static void main(String[] args) {
+
+		// create cart
+		Cart anOrder = new Cart();
+
+		// create and add dvds
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		anOrder.addDigitalVideoDisc(dvd1);
+
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		anOrder.addDigitalVideoDisc(dvd2);
+
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+		anOrder.addDigitalVideoDisc(dvd3);
+
+		// print cost
+		System.out.print("Total cost: ");
+		System.out.println(anOrder.totalCost());
+
+		// remove dvd
+		anOrder.removeDigitalVideoDisc(dvd1);
+	}
+}

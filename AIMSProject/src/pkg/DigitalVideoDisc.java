bddb807b7d9d804bf14b3ab074f9d0dfe9db148e
Hwi <nguyenquochuy712@gmail.com>
2023-05-27 09:03:00 +0700

    Lab03.5/classifier-member

--- AIMSProject/src/pkg/DigitalVideoDisc.java
+++ AIMSProject/src/pkg/DigitalVideoDisc.java
@@ -1,5 +1,7 @@
 package pkg;
 public class DigitalVideoDisc {
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
     private String title;
     private String category;
 	private String director;
@@ -27,6 +29,8 @@ public class DigitalVideoDisc {
     public DigitalVideoDisc(String title) {
     	super();
         this.title = title;
+        nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String title, String category, float cost) {
@@ -34,6 +38,8 @@ public class DigitalVideoDisc {
         this.title = title;
         this.category = category;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String title, String category, String director, float cost) {
@@ -42,6 +48,8 @@ public class DigitalVideoDisc {
         this.category = category;
         this.director = director;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -51,6 +59,8 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
     }
     
 	public void setTitle(String title) {
